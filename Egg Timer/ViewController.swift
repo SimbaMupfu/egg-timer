9170fd764fd9f661b2e357d9afd4c1a541a4d793
simba_mupfu <scmupfururirwa@gmail.com>
2023-06-19 16:48:19 +0200

    Use a dictionary to calculate the times.

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text
+        if(hardness != nil){
+            print(eggTimes[hardness!])
+        }
+    }
+    
 }
 
