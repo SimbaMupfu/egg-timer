edaa9dc58f1aa9c1709ae977453b7d759c60e6e7
simba_mupfu <scmupfururirwa@gmail.com>
2023-06-19 15:53:49 +0200

    Create an egg timer app

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Simbarashe Mupfururirwa on 2023/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
